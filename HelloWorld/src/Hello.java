cec8031094061e463341aaa2f08fd4f8f255896e
ambalika07 <ambalika.katoch07@gmail.com>
2017-11-26 15:54:50 -0800

    HelloWorld commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Ambalika!");
+
+        int myFirstNumber = (5 + 10) + (2*13);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int newTotal = 1000 - myTotal;
+
+        System.out.println(newTotal);
+
+    }
+}
