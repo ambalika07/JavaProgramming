792feea120919e456d4ffe288ebf690c0a5528e1
ambalika07 <ambalika.katoch07@gmail.com>
2017-11-27 22:13:01 -0800

    CharAndBoolean

---
+++ CharAnadBoolean/src/com/katoch/Main.java
@@ -0,0 +1,15 @@
+package com.katoch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char myRegister = '\u00AE';
+        System.out.println("The register symbols is : " + myRegister);
+    }
+}
