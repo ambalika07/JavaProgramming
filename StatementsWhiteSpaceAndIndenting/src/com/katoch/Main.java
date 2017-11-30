a461ce9415505d6e2ba5c3a077023669eb4f3956
ambalika07 <ambalika.katoch07@gmail.com>
2017-11-30 09:20:15 -0700

    statements, white spaces and indenting first commit

---
+++ StatementsWhiteSpaceAndIndenting/src/com/katoch/Main.java
@@ -0,0 +1,18 @@
+package com.katoch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVariable = 50;
+	    myVariable ++;
+	    myVariable --;
+        System.out.println("This is a test");
+
+        //white space: space between datatypes or variables.
+
+        int myVariable1 = 50;
+        if (myVariable1 == 50){
+            System.out.println("Printed");
+        }
+    }
+}
