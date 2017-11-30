a8d65388db36ae3406424362d3ea72a7167fefb5
ambalika07 <ambalika.katoch07@gmail.com>
2017-11-29 23:02:14 -0700

    operators second commit

--- Operators/src/com/katoch/Main.java
+++ Operators/src/com/katoch/Main.java
@@ -51,8 +51,37 @@ public class Main {
             System.out.println("You got the high score!");
 
         int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100)
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than second top score and less than 100");
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true");
+
+        int newValue = 50;
+        if(newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = false;
+        if (isCar)
+            System.out.println("This is not suppose to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
+        double newVariable = 20;
+        double secondVariable = 80;
+        double newResult = (newVariable + secondVariable) * 25;
+        System.out.println("The result is = " + newResult);
+        double newRemainder = newResult % 40;
+        System.out.println("the remainder is = " + newRemainder);
+
+        if (newResult >= 2500)
+            System.out.println("Total was over the limit");
+
+        if (newRemainder <= 20 )
+            System.out.println("Remainder is less than 20");
 
 
     }
