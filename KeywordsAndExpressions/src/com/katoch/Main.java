bef221d77ea83ce3d81d1bdf39d8fed1a1c88de0
ambalika07 <ambalika.katoch07@gmail.com>
2017-11-30 08:55:35 -0700

    Keywords and expressions first commit

---
+++ KeywordsAndExpressions/src/com/katoch/Main.java
@@ -0,0 +1,12 @@
+package com.katoch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int score = 100; // expression (next to datatype)
+	if (score > 90){ // expression (next to datatype)
+        System.out.println("You got the high score!"); //expression (next to sout)
+        score = 0; //expression
+    }
+    }
+}
